38be4d9f6d0e715ecc3b8d828064e524e84cc45c
Carlos Garcia <garlos.figueroa@gmail.com>
2016-09-27 18:37:52 -0400

    elementos no usables

--- assets/js/controller/appController.js
+++
@@ -1,9 +0,0 @@
-(function( win, factory ){ factory( win ); }( this, function( win ) {
-win.angular.module( 'app' )
-.controller('appController', [
-	         '$state',
-	function( $state ) {
-		
-	}
-])}));
-
